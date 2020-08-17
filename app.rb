be161b6ff61e06a409f077f691f3de7ed3f8062c
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-08-17 15:43:13 +0700

    Add ssqlite varibal

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+db = SQLite3::Database.new 'barbershop.db'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
